9c09b9cbea82f441cf77eda76b0ea2d546aa2a40
Jeff Fennell <jfennell@lion.lmu.edu>
2016-01-30 21:02:20 -0800

    Set up unit testing for api.

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,13 @@
+require('dotenv').load();
+
 var express    = require('express');
-var dotenv     = require('dotenv');
 var morgan     = require('morgan');
 var bodyParser = require('body-parser');
 var jwt        = require('jwt-simple');
 var fs         = require('fs');
 var https      = require('https');
 var http       = require('http');
-
-var tokens = require('./handlers/tokens');
+var tokens     = require('./handlers/tokens');
 
 var app = express();
 
@@ -15,25 +15,34 @@ var privateKey  = fs.readFileSync(`${__dirname}../../certs/server.key`);
 var certificate = fs.readFileSync(`${__dirname}../../certs/server.crt`);
 var credentials = {key:privateKey, cert:certificate};
 
-dotenv.load();
-
 var HTTP_PORT          = process.env.HTTP_PORT      || 8080;
 var HTTPS_PORT         = process.env.HTTPS_PORT     || 443;
 var LOGGING_LEVEL      = process.env.LOGGING_LEVEL  || 'combined';
 
-var methodNotAllowed = (req, res) => res.status(405).json({message: 'method not allowed'});
+var methodNotAllowed = (req, res) => res.status(405)
+                                        .json({message: 'method not allowed'});
 
 //unauthenticated routes
-app.get('/api/v1/tokens', tokens.register);
+app.get('/api/v1/tokens', tokens.generate);
 app.post('/api/v1/tokens', tokens.refresh);
-app.all('/api/v1/tokens', methodNotAllowed)
+app.all('/api/v1/tokens', methodNotAllowed);
 
 //authenticated routes
+// app.get('/api/v1/user/:userid/groups', groups.getMembers);
+// app.put('/api/v1/user/:userid/groups', groups.addMembers);
+
+// app.get('api/v1/user/:userid/images', images.retrieve);
+// app.get('api/v1/user/:userid/images', images.save);
 
 app.use(morgan(LOGGING_LEVEL));
 
 http.createServer(app).listen(HTTP_PORT);
 https.createServer(credentials, app).listen(HTTPS_PORT);
 
-console.log(`http server listening on port ${HTTP_PORT}`);
-console.log(`https server listening on port ${HTTPS_PORT}`);
+if (process.env.NODE_ENV !== 'test') {
+	console.log(`http server listening on port ${HTTP_PORT}`);
+	console.log(`https server listening on port ${HTTPS_PORT}`);
+}
+
+
+module.exports = app;
--- server/test/test.js
+++ server/test/test.js
@@ -1,12 +1,65 @@
-var request = require('supertest');
-var should  = require('should');
-var dotenv  = require('dotenv');
+process.env.NODE_ENV = 'test';
 
+var supertest = require('supertest');
+var chai      = require('chai');
+var dotenv    = require('dotenv');
+var api       = require('../src/server.js');
+
+var request   = supertest(api);
 dotenv.load();
-var port = process.env.API_port;
 
-describe('Capstone api') => {
+describe('Capstone api', function() {
 
-	it('Returns 200 response code when it receives a token request', done => {
-		request(`http://localhost:${port}`)
-	})
+	describe('/api/v1/tokens', function() {
+		context('When it receives an unsupported method', function() {
+			it('returns 405 response code', function() {
+				
+			});
+		})	
+		context('POST', function() {
+			context('When request is valid', function() {
+
+			it('returns 200 response code', function() {
+		
+			});
+
+			it('returns a new token', function() {
+
+			});
+
+			it('returns 401 status code if account already exists with the same username', function() {
+
+			});
+
+			});
+
+			context('When request is invalid', function() {
+				context('When username missing', function() {
+					it('returns 400 status code', function() {
+
+					});
+				});
+
+				context('When password missing', function() {
+					it('returns 400 status code', function() {
+
+					});	
+				});
+
+				context('When password is length 0', function() {
+					it('returns 400 status code', function() {
+
+					});
+				})
+
+				context('When username is length 0', function() {
+					it('returns 400 status code', function() {
+						
+					});
+				})
+			});
+
+		})
+	});
+
+});
