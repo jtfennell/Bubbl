880093c2f6a2192008cda0f0028432b589f6d8d3
Jeff Fennell <jfennell@lion.lmu.edu>
2016-02-29 18:16:54 -0800

    Add better feedback for db redeploy.

--- scripts/db-redeploy.js
+++ scripts/db-redeploy.js
@@ -4,13 +4,12 @@ var dropAllTables = fs.readFileSync('./database/dropSchema.sql').toString();
 var setUpSchema   = fs.readFileSync('./database/schema.sql').toString();
 var database      = require('../database/pg-client.js');
 
-
 console.log('Redeploying database...');
 database.query(dropAllTables + setUpSchema, (err, result) => {
     if (err) {
         console.log(err);
     };
-});
 
-console.log('Database deployed.'); 
-process.exit();
+    console.log('Database deployed.'); 
+    process.exit();
+});
