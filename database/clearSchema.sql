97cf2c3a11bd8701d07b31f3c9ef1a5ee09b39b5
Jeff Fennell <jfennell@lion.lmu.edu>
2016-04-04 19:37:36 -0700

    Update delete and clear schema for tests.

--- database/clearSchema.sql
+++ database/clearSchema.sql
@@ -1,8 +1,12 @@
 ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
 ALTER SEQUENCE groups_group_id_seq RESTART WITH 1;
 ALTER SEQUENCE user_invited_to_group_invite_id_seq RESTART WITH 1;
+ALTER SEQUENCE images_image_id_seq RESTART WITH 1;
 
 DELETE FROM group_contains_user;
 DELETE FROM user_invited_to_group;
 DELETE FROM users;
 DELETE FROM groups;
+DELETE FROM user_picks_profile_image;
+DELETE FROM group_contains_image;
+DELETE FROM images;
--- database/dropSchema.sql
+++ database/dropSchema.sql
@@ -2,3 +2,6 @@ DROP TABLE IF EXISTS group_contains_user;
 DROP TABLE IF EXISTS user_invited_to_group;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS group_contains_image;
+DROP TABLE IF EXISTS user_picks_profile_image;
+DROP TABLE IF EXISTS images;
